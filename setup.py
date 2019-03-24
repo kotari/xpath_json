eef11b273c3b5a91d906246d6c3ec61378bb5d5e
Rajesh Kotari <kotari@rajeshcentos.lan>
2019-03-24 07:51:24 -0700

    updating github path

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Implementing xpath like search for JSON in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/kotari/xpath_json",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
